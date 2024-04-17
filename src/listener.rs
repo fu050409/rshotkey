118e65a32f0ac391d1848d1dc0a174af9b36c066
苏向夜 <fu050409@163.com>
2024-04-17 23:11:29 +0800

    feaT(listener): use hook func

--- src/listener.rs
+++ src/listener.rs
@@ -1,14 +1,24 @@
-use std::process;
+use std::{process, time::Duration};
 
 use anyhow::Result;
-use futures::future::BoxFuture;
-use rdev::{listen, Event};
+use futures::{future::BoxFuture, TryFutureExt};
+use rdev::{listen, Event, EventType};
 use tokio::sync::mpsc;
 
-type Hook = fn() -> BoxFuture<'static, Result<()>>;
+pub type HookResult = BoxFuture<'static, Result<()>>;
+pub type Hook = fn() -> HookResult;
 
-#[derive(Debug, Clone, Copy)]
-pub struct HotKey {}
+#[derive(Debug, Clone)]
+pub struct HotKey {
+    pub key: Vec<EventType>,
+    pub delay_time: Duration,
+}
+
+impl HotKey {
+    pub fn new(key: Vec<EventType>, delay_time: Duration) -> Self {
+        Self { key, delay_time }
+    }
+}
 
 #[derive(Clone)]
 pub struct Listener {
@@ -16,10 +26,52 @@ pub struct Listener {
     hooks: Vec<(Vec<HotKey>, Hook)>,
 }
 
-pub async fn hook(history: Vec<Event>, hooks: Vec<(Vec<HotKey>, Hook)>) {
-    for (hot_key, hook) in hooks {
-
+pub async fn hook(history: Vec<Event>, hooks: Vec<(Vec<HotKey>, Hook)>) -> Result<()> {
+    for (hot_keys, hook) in hooks {
+        let mut checked = true;
+        for hot_key in hot_keys {
+            let mut checks = Vec::new();
+            let mut keys = hot_key.key.clone();
+            if history.len() < hot_key.key.len() {
+                checked = false;
+                break;
+            }
+            history[history.len() - hot_key.key.len()..].clone_into(&mut checks);
+            let mut matched = true;
+            while !checks.is_empty() {
+                let mut found = false;
+                let mut check_idx = 0;
+                while check_idx < checks.len() {
+                    let mut key_idx = 0;
+                    while key_idx < keys.len() {
+                        if checks[check_idx].event_type == keys[key_idx] {
+                            found = true;
+                            checks.remove(check_idx);
+                            keys.remove(key_idx);
+                            break;
+                        }
+                        key_idx += 1;
+                    }
+                    if found {
+                        break;
+                    }
+                    check_idx += 1;
+                }
+                if !found {
+                    matched = false;
+                    break;
+                }
+            }
+            if !matched {
+                checked = false;
+                break;
+            }
+        }
+        if checked {
+            hook().await?;
+        }
     }
+    Ok(())
 }
 
 impl Listener {
@@ -52,7 +104,10 @@ impl Listener {
         loop {
             if let Some(event) = rx.recv().await {
                 self.history.push(event);
-                tokio::spawn(hook(self.history.clone(), self.hooks.clone()));
+                tokio::spawn(
+                    hook(self.history.clone(), self.hooks.clone())
+                        .unwrap_or_else(|e| eprint!("{}", e)),
+                );
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,138 @@
 pub mod listener;
 
+use std::time::Duration;
+
 use anyhow::Result;
-use rdev::{listen, Button, EventType};
-use tokio::sync::mpsc;
-use tokio::time::{timeout, Duration};
+use futures::FutureExt;
+use listener::{HookResult, HotKey, Listener};
+// use rdev::{listen, Button, EventType};
+// use tokio::sync::mpsc;
+// use tokio::time::{timeout, Duration};
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let (tx, mut rx) = mpsc::unbounded_channel();
+//     tokio::spawn(async move {
+//         listen(move |event| {
+//             tx.send(event)
+//                 .unwrap_or_else(|e| eprintln!("Could not send event {:?}", e));
+//         })
+//         .expect("Could not listen");
+//     });
+
+//     loop {
+//         let event = match rx.recv().await {
+//             Some(event) => event,
+//             None => {
+//                 continue;
+//             }
+//         };
+//         match event.event_type {
+//             rdev::EventType::ButtonPress(rdev::Button::Left) => {
+//                 let next_event = loop {
+//                     match timeout(Duration::from_secs_f64(0.15), rx.recv()).await {
+//                         Ok(Some(event)) => match event.event_type {
+//                             EventType::ButtonRelease(Button::Left) => continue,
+//                             EventType::ButtonPress(Button::Left) => {
+//                                 break Some(event);
+//                             }
+//                             _ => continue,
+//                         },
+//                         Err(_) => {
+//                             break None;
+//                         }
+//                         _ => continue,
+//                     };
+//                 };
+//                 if next_event.is_some() {
+//                     let duration = next_event
+//                         .unwrap()
+//                         .time
+//                         .duration_since(event.time)?
+//                         .as_secs_f64();
+//                     if duration < 0.15 {
+//                         println!("double click!");
+//                     } else {
+//                         println!("clicked once!")
+//                     }
+//                 } else {
+//                     println!("clicked once!")
+//                 }
+//             }
+//             _ => {}
+//         }
+//     }
+// }
+
+fn clicked() -> HookResult {
+    async move {
+        println!("左键被按下");
+        Ok(())
+    }
+    .boxed()
+}
+
+fn press_c() -> HookResult {
+    async move {
+        println!("C被按下");
+        Ok(())
+    }
+    .boxed()
+}
+
+fn press_ctrl_d() -> HookResult {
+    async move {
+        println!("Ctrl+D被按下");
+        Ok(())
+    }
+    .boxed()
+}
+
+fn double_clicked() -> HookResult {
+    async move {
+        println!("鼠标双击");
+        Ok(())
+    }
+    .boxed()
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (tx, mut rx) = mpsc::unbounded_channel();
-    tokio::spawn(async move {
-        listen(move |event| {
-            tx.send(event)
-                .unwrap_or_else(|e| eprintln!("Could not send event {:?}", e));
-        })
-        .expect("Could not listen");
-    });
+    // for i in 0..10 {
+    //     println!("{}", i);
+    // }
+    env_logger::init();
+    // let route_button_left = vec![HotKey::new(
+    //     vec![rdev::EventType::ButtonPress(rdev::Button::Left)],
+    //     Duration::from_secs(0),
+    // )];
+    let route_c = vec![HotKey::new(
+        vec![rdev::EventType::KeyPress(rdev::Key::KeyC)],
+        Duration::from_secs(0),
+    )];
+    let ctrl_d = vec![HotKey::new(
+        vec![
+            rdev::EventType::KeyPress(rdev::Key::KeyD),
+            rdev::EventType::KeyPress(rdev::Key::ControlLeft),
+        ],
+        Duration::from_secs(0),
+    )];
+    let double_click = vec![
+        HotKey::new(
+            vec![rdev::EventType::ButtonPress(rdev::Button::Left)],
+            Duration::from_secs_f64(0.15),
+        ),
+        HotKey::new(
+            vec![rdev::EventType::ButtonRelease(rdev::Button::Left)],
+            Duration::from_secs_f64(0.15),
+        ),
+    ];
 
-    loop {
-        let event = match rx.recv().await {
-            Some(event) => event,
-            None => {
-                continue;
-            }
-        };
-        match event.event_type {
-            rdev::EventType::ButtonPress(rdev::Button::Left) => {
-                let next_event = loop {
-                    match timeout(Duration::from_secs_f64(0.15), rx.recv()).await {
-                        Ok(Some(event)) => match event.event_type {
-                            EventType::ButtonRelease(Button::Left) => continue,
-                            EventType::ButtonPress(Button::Left) => {
-                                break Some(event);
-                            }
-                            _ => continue,
-                        },
-                        Err(_) => {
-                            break None;
-                        }
-                        _ => continue,
-                    };
-                };
-                if next_event.is_some() {
-                    let duration = next_event
-                        .unwrap()
-                        .time
-                        .duration_since(event.time)?
-                        .as_secs_f64();
-                    if duration < 0.15 {
-                        println!("double click!");
-                    } else {
-                        println!("clicked once!")
-                    }
-                } else {
-                    println!("clicked once!")
-                }
-            }
-            _ => {}
-        }
-    }
+    let mut listener = Listener::new();
+    // listener.register(route_button_left, clicked);
+    listener.register(route_c, press_c);
+    listener.register(ctrl_d, press_ctrl_d);
+    listener.register(double_click, double_clicked);
+    listener.listen().await?;
+    Ok(())
 }
